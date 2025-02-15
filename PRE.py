69b19472a6ad58709913c4848ed42a065d1d603f
coro-afk <cxhaotianyin@gmail.com>
2025-02-15 14:11:45 +0800

    add PRE.py

--- PRE.py
+++ PRE.py
@@ -13,37 +13,23 @@ class KyberParameterSet:
         self.du = du    # 2^(bits in the first ciphertext)
         self.dv = dv    # 2^(bits in the second ciphertext)
 
-
-def Kyber_to_MLWE(kps):
-    # if kps.eta1 != kps.eta2:
-    #     raise "The security script does not handle different error parameter in secrets and errors (eta1 != eta2) "
-
-    # Check whether ciphertext error variance after rounding is larger than secret key error variance
-    Rc = build_mod_switching_error_law(kps.q, 2**kps.du)
-    var_rounding = sum([i*i*Rc[i] for i in Rc.keys()])
-
-    # if kps.eta2/2. + var_rounding < kps.eta2/2.:
-    #     raise "The security of the ciphertext MLWE may not be stronger than the one of the public key MLWE"    
-
-    return MLWEParameterSet(kps.n, kps.k, kps.k + 1, kps.eta1, kps.q)
-
-
 def communication_costs(ps):
     """ Compute the communication cost of a parameter set
     :param ps: Parameter set (ParameterSet)
-    :returns: (cost_Alice, cost_Bob) (in Bytes)
+    :returns: costs in bytes (dict)
     """
-    A_space = 256 + ps.n * ps.k * ps.du
-    B_space = ps.n * ps.k * ps.du + ps.n * ps.dv
-    return (int(round(A_space))/8., int(round(B_space))/8.)
-
+    result = {}
+    com_sk = 12 * ps.k * ps.n / 8  # 12 bytes per element (Encode_12)
+    result["com_pk"] = 12 * ps.k * ps.n / 8 + 32
+    result["com_ct"] = ps.du * ps.k * ps.n / 8 + ps.dv * ps.n / 8
+    result["com_rk"] = 12 * ps.k * ps.n / 8 + 12 * ps.n / 8
+    return result
 
 def summarize(ps):
-    print ("params: ", ps.__dict__)
-    print ("com costs: ", communication_costs(ps))
+    print("params: ", ps.__dict__)
+    print("com costs: ", communication_costs(ps))
     F, f = p2_cyclotomic_error_probability(ps)
-    print ("failure: %.1f = 2^%.1f"%(f, log(f + 2.**(-300))/log(2)))
-
+    print("failure: %.1f = 2^%.1f" % (f, log(f + 2.**(-300)) / log(2)))
 
 if __name__ == "__main__":
     # Parameter sets
@@ -52,23 +38,20 @@ if __name__ == "__main__":
     ps_paranoid = KyberParameterSet(256, 3329, 4, 2, 2, 11, 5)
 
     # Analyses
-    print ("PRE512 (light):")
-    print ("--------------------")
-    print ("security:")
-    MLWE_summarize_attacks(Kyber_to_MLWE(ps_light))
+    print("PRE512 (light):")
+    print("--------------------")
+    print("security:")
     summarize(ps_light)
-    print ()
+    print()
 
-    print ("PRE768 (recommended):")
-    print ("--------------------")
-    print ("security:")
-    MLWE_summarize_attacks(Kyber_to_MLWE(ps_recommended))
+    print("PRE768 (recommended):")
+    print("--------------------")
+    print("security:")
     summarize(ps_recommended)
-    print ()
+    print()
 
-    print ("PRE1024 (paranoid):")
-    print ("--------------------")
-    print ("security:")
-    MLWE_summarize_attacks(Kyber_to_MLWE(ps_paranoid))
+    print("PRE1024 (paranoid):")
+    print("--------------------")
+    print("security:")
     summarize(ps_paranoid)
-    print ()
+    print()
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# security-estimates
+# security-estimates for a kyber-based PRE scheme
+
+Kyber.py tests the communication costs and failure rate of cpapke.
+PRE.py tests the communication costs and failure rate of re-encrypted ciphertexts.
